0d5c5314ddb4a4cf637d7e1db5b5a60d639f9c88
Patrick Sherrard <patsherrard@gmail.com>
2020-05-01 16:46:17 -0400

    Updates

---
+++ precache-manifest.bab2000593f7c1270f7d97e996712a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0256ffb5c3c699cfef8b2bb67fee4f",
+    "url": "/patrickSherrardProject5/index.html"
+  },
+  {
+    "revision": "9bf6f4a07faee9d52712",
+    "url": "/patrickSherrardProject5/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "cb3b499dbc2b835d0cbb",
+    "url": "/patrickSherrardProject5/static/js/2.d3f7268b.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/patrickSherrardProject5/static/js/2.d3f7268b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf6f4a07faee9d52712",
+    "url": "/patrickSherrardProject5/static/js/main.489e1f3d.chunk.js"
+  },
+  {
+    "revision": "1f9f44a8280ccb1ec84c",
+    "url": "/patrickSherrardProject5/static/js/runtime-main.d99057b9.js"
+  }
+]);
