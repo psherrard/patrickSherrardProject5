6b42dc9bce83c90d8d930b2057d866f7d0e93b7f
Patrick Sherrard <patsherrard@gmail.com>
2020-05-03 19:54:43 -0400

    getting there...ass SASS?

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -22,7 +22,7 @@ class LandingPage extends Component{
                 {this.state.description
                     ?   <div className="landingPage">
                             <h2>DESCRIPTION</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Corrupti aperiam quasi ipsa dolore quis sint quam doloribus fugit labore iure quos maiores sit temporibus laudantium doloremque debitis molestiae commodi error.</p>
+                            <p>Welcome to Weather Synth. This app use’s real time weather data from different cities across the world to shape sound. As the user, you get to choose what city you want to listen to. What you’re listening to is varius weather parameters affecting the synth by picking which notes are played and in what scale. As well as the amount of reverb, chorus, phaser and delay. The speed of the song is determined by the wind speed and the oscillator by the weather condition. Press start to hear the end result. </p>
                             <button onClick={this.onClickLandingPage}>Enter</button>
                         </div>
                     :   ''
