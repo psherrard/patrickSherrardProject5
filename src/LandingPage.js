642bdff0cfc4646a5164169f3adabbdf3151d201
Patrick Sherrard <patsherrard@gmail.com>
2020-06-08 21:18:55 -0400

    ironing out the details

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -27,7 +27,7 @@ class LandingPage extends Component{
                             <p>This app collects real time data from different cities across the world and composes it into a soundscape that signifies the active weather system present in your selected city. Various weather parameters affect the synth by impacting which notes are played and in what scale. Similar to a wind chime, the speed of wind determines the pace of the composition.</p>
                             <ul>
                                 <li>I: Choose a city</li>
-                                <li>II: Launch the city</li>
+                                <li>II: Load City</li>
                                 <li>III: Press START to hear melody</li>
                                 <li>IV: Press STOP to stop the melody and choose new city</li>
                             </ul>
